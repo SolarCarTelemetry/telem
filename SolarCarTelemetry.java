fabd9a353b57a9fde7be274d6603e4902782eba8
Arij Saleem <arijsaleem@gmail.com>
2018-03-05 00:47:25 -0500

    Update SolarCarTelemetry.java
    Added mock layout for left pane

--- SolarCarTelemetry.java
+++ SolarCarTelemetry.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package solarcartelemetry;
 
 import javafx.application.*;
 import javafx.event.*;
 import javafx.scene.text.*;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
+import javafx.scene.shape.*;
 import javafx.stage.*;
 
 
@@ -35,7 +32,13 @@ public class SolarCarTelemetry extends Application{
         
         //Data Pane
         TilePane DataPane=new TilePane();
-        DataPane.setStyle("-fx-background-color: #CD5C5C;"); //use this to determine the size of your pane
+        DataPane.setStyle("-fx-background-color: linear-gradient(to bottom, #924444, #E87373);"
+                + "-fx-background-radius: 0px 0px 6px 6px;" 
+                ); //use this to determine the size of your pane and other things
+        DataPane.setPrefHeight(60);
+        DataPane.setHgap(130);
+        
+        //Text objects in DataPane
         Text speed = new Text("Speed:");
         Text RPM= new Text("RPM:");
         Text PConsume= new Text("P.Consumption:");
@@ -48,31 +51,40 @@ public class SolarCarTelemetry extends Application{
         CVoltage.setFont(Font.font("Arial",14));
         RunTime.setFont(Font.font("Arial",14));
         Status.setFont(Font.font("Arial",14));
-        /* May need to use this not sure yet
-        speed.setTextAlignment(TextAlignment.JUSTIFY);
-        RPM.setTextAlignment(TextAlignment.JUSTIFY);
-        PConsume.setTextAlignment(TextAlignment.JUSTIFY);
-        CVoltage.setTextAlignment(TextAlignment.JUSTIFY);
-        RunTime.setTextAlignment(TextAlignment.JUSTIFY);
-        Status.setTextAlignment(TextAlignment.JUSTIFY);
-        */
+        
+        //Centers all the text objects
+        speed.setTextAlignment(TextAlignment.CENTER);
+        RPM.setTextAlignment(TextAlignment.CENTER);
+        PConsume.setTextAlignment(TextAlignment.CENTER);
+        CVoltage.setTextAlignment(TextAlignment.CENTER);
+        RunTime.setTextAlignment(TextAlignment.CENTER);
+        Status.setTextAlignment(TextAlignment.CENTER);
+        
         //Add data to pane
         DataPane.getChildren().addAll(speed,RPM,PConsume,CVoltage,RunTime,Status);
         
         //Map Pane
         StackPane MapPane=new StackPane();
-        
+        MapPane.setStyle("-fx-background-color: #CBCBCB;");
+        Rectangle demo = new Rectangle(420, 300);
+        Text demoText = new Text("MAP GOES HERE");
+        demoText.setFill(Color.WHITE); 
+        demo.setFill(Color.BLACK); 
+        demoText.setTranslateY(demo.getY() - demo.getHeight()/2);
+        MapPane.getChildren().addAll(demo, demoText);
+        MapPane.setAlignment(Pos.BOTTOM_CENTER);
         //Graphs Pane
         StackPane GraphPane=new StackPane();
         
         //Add all the panes to the MainPane
         //Set the scene as the mainpane and show it on the stage(Frame)
         MainPane.setTop(DataPane);
-        MainPane.setRight(MapPane);
-        MainPane.setLeft(GraphPane);
+        MainPane.setRight(GraphPane);
+        MainPane.setLeft(MapPane);
         Scene Main=new Scene(MainPane,1280,720);
         window.setScene(Main);
         window.show();
+        primaryStage.setFullScreen(true);   //Automatically launches in fullscreen
     }
       
     public static void main(String[] args) {
